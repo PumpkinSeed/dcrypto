7d58024c330af29d659f57f5481f86ae3ecfa1b8
PumpkinSeed <qwer.kocka@gmail.com>
2020-12-18 13:38:03 +0100

    Add tests to cover all possibilites for crypto

--- test/dcrypto_test.dart
+++ test/dcrypto_test.dart
@@ -3,21 +3,69 @@ import 'package:test/test.dart';
 import 'dart:convert';
 
 void main() {
-  group('A group of tests', () {
+  test('Base64 plaintext and pure key', () {
     var c = Crypto(AES_CBC_128('2271z4734hxepwb6'));
     c.keyType = TextType.pure;
 
     var key = 'xeq1m8i959r3bwgu';
-    var plaintextBase64 = 'bTUxdWFxZGcyNjNhNW53Mw=='; // m51uaqdg263a5nw3
-    var ciphertext = c.encrypt(plaintextBase64, key);
+    var plaintext = 'bTUxdWFxZGcyNjNhNW53Mw=='; // m51uaqdg263a5nw3
+    var ciphertext = c.encrypt(plaintext, key);
 
     var decrypted = c.decrypt(ciphertext, key);
-    print('text: $decrypted');
     var decodedText = utf8.decode(base64.decode(decrypted));
-    print('text: $decodedText');
+    expect(decodedText, 'm51uaqdg263a5nw3');
+  });
 
-    if (decodedText != 'm51uaqdg263a5nw3') {
-      print('ERROR');
-    }
+  test('Pure plaintext and pure key', () {
+    var c = Crypto(AES_CBC_128('2271z4734hxepwb6'));
+    c.keyType = TextType.pure;
+    c.plaintextType = TextType.pure;
+
+    var key = 'xeq1m8i959r3bwgu';
+    var plaintext = 'm51uaqdg263a5nw3';
+    var ciphertext = c.encrypt(plaintext, key);
+
+    var decrypted = c.decrypt(ciphertext, key);
+    expect(decrypted, 'm51uaqdg263a5nw3');
+  });
+
+  test('Pure plaintext as input, base64 plaintext as output and pure key', () {
+    var c = Crypto(AES_CBC_128('2271z4734hxepwb6'));
+    c.keyType = TextType.pure;
+    c.plaintextType = TextType.pure;
+
+    var key = 'xeq1m8i959r3bwgu';
+    var plaintext = 'm51uaqdg263a5nw3';
+    var ciphertext = c.encrypt(plaintext, key);
+
+    c.plaintextType = TextType.base64;
+    var decrypted = c.decrypt(ciphertext, key);
+    var decodedText = utf8.decode(base64.decode(decrypted));
+    expect(decodedText, 'm51uaqdg263a5nw3');
+  });
+
+  test('Base64 plaintext as input, pure plaintext as output and pure key', () {
+    var c = Crypto(AES_CBC_128('2271z4734hxepwb6'));
+    c.keyType = TextType.pure;
+
+    var key = 'xeq1m8i959r3bwgu';
+    var plaintext = 'bTUxdWFxZGcyNjNhNW53Mw=='; // m51uaqdg263a5nw3
+    var ciphertext = c.encrypt(plaintext, key);
+
+    c.plaintextType = TextType.pure;
+    var decrypted = c.decrypt(ciphertext, key);
+    expect(decrypted, 'm51uaqdg263a5nw3');
+  });
+
+  test('Base64 plaintext and base64 key', () {
+    var c = Crypto(AES_CBC_128('2271z4734hxepwb6'));
+
+    var key = 'eGVxMW04aTk1OXIzYndndQ=='; // xeq1m8i959r3bwgu
+    var plaintext = 'bTUxdWFxZGcyNjNhNW53Mw=='; // m51uaqdg263a5nw3
+    var ciphertext = c.encrypt(plaintext, key);
+
+    var decrypted = c.decrypt(ciphertext, key);
+    var decodedText = utf8.decode(base64.decode(decrypted));
+    expect(decodedText, 'm51uaqdg263a5nw3');
   });
 }
